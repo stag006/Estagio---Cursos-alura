f66951a23edfac18e400f5e2842fbc71c9cee58e
Stag06 <stag006@benner.com.br>
2019-05-31 12:26:36 -0300

    comitando o projeto do curso 3-CSharp

---
+++ T01/3-CSharp/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        public static int TotalDeFuncionarios { get; private set; }
+
+        public string Nome { get; set; }
+        public string CPF { get; private set; }
+        public double Salario { get; protected set; }
+        
+        public Funcionario(double salario, string cpf)
+        {
+            Console.WriteLine("Criando FUNCIONARIO");
+
+            CPF = cpf;
+
+            TotalDeFuncionarios++;
+        }
+
+        public void AumentarSalario()
+        {
+            // Salario = Salario + (Salario * 0.1);
+            // Salario = Slario 1.1;
+            Salario *= 1.1;
+        }
+
+        public virtual double GetBonificacao()
+        {
+            return Salario * 0.10;
+        }
+    }
+}
