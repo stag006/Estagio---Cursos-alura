06276ed15e42cab0a4cb6cdb9d8ef7d33de30ef6
Stag06 <stag006@benner.com.br>
2019-06-07 11:07:15 -0300

    comitando a aula 3

--- T01/Entity/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T01/Entity/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -11,36 +11,7 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
-                var produtos = contexto.Produtos.ToList();
 
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                var novoProduto = new Produto()
-                {
-                    Nome = "Desinfetante",
-                    Categoria = "Limpeza",
-                    Preco = 2.99
-                };
-                contexto.Produtos.Add(novoProduto);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                contexto.SaveChanges();
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-            }
-        }
-
-        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
-        {
-            Console.WriteLine("==============");
-            foreach (var e in entries)
-            {
-                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
-            }
         }
     }
 }
