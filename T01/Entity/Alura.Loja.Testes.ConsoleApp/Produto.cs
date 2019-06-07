40bd87e5c70ab5abf946c8e0927bbd95af2c02e9
Stag06 <stag006@benner.com.br>
2019-06-07 11:54:19 -0300

    ]

--- T01/Entity/Alura.Loja.Testes.ConsoleApp/Produto.cs
+++ T01/Entity/Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -5,7 +5,7 @@
         public int Id { get; internal set; }
         public string Nome { get; internal set; }
         public string Categoria { get; internal set; }
-        public double PrecoUnitario { get; internal set; }
+        public double PrecoUnitario{ get; internal set; }
         public string Unidade { get; internal set; }
 
         public override string ToString()
