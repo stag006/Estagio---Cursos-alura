e3fd982dcd377de47766988b21e97b212bbb4be2
Arthur Teixeira <stag006@benner.com.br>
2019-06-25 22:53:38 -0300

    criando algumas classes

---
+++ T02/Design Patterns C# I/CursoDesignPatterns/Teste.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+    class Teste
+    {
+        static void Main(String[] args)
+        {
+            Orcamento reforma = new Orcamento(500.0);
+
+            Imposto novoImposto = new ICCC();
+            Console.WriteLine(novoImposto.Calcula(reforma));
+        }
+    }
+}
