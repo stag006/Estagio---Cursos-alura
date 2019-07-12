799c6aa9498891a6b0173a2b7671c9cfadf64e73
Arthur Teixeira <stag006@benner.com.br>
2019-07-12 01:56:40 -0300

    escrevendo o codigo de suporte http

---
+++ T02/C# Reflection parte 1 - Metadados do seu codigo.NET/ByteBank.Portal/ByteBank.Portal/Infraestrutura/WebApplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Infraestrutura
+{
+    public class WebApplication
+    {
+        // http://bytebank.com/ ?
+        // http://localhost:80/ ?
+        public void Iniciar()
+        {
+            var httpListener = new HttpListener();
+
+            httpListener.Start();
+
+        }
+    }
+}
---
+++ T02/C# Reflection parte 1 - Metadados do seu codigo.NET/ByteBank.Portal/ByteBank.Portal/Program.cs
@@ -0,0 +1,21 @@
+﻿using ByteBank.Portal.Infraestrutura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webApplication = new WebApplication();
+            webApplication.Iniciar();
+        }
+    }
+}
+
+
+
