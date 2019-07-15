5bfa8dfe5420a1aa5370e0ea504c400a8a10c565
Arthur Teixeira <stag006@benner.com.br>
2019-07-15 10:20:40 -0300

    comit da primeira e segunda aula juntas

---
+++ T03/JavaScript/introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -0,0 +1,18 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = document.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdaltura = paciente.querySelector(".info-altura");
+var altura = tdaltura.textContent;
+
+
+console.log(paciente); // tr
+console.log(tdPeso); // td que tem o peso
+console.log(peso); // Obter 100
+console.log(tdaltura); // td que tem o peso
+console.log(altura); // Obter 100
+
