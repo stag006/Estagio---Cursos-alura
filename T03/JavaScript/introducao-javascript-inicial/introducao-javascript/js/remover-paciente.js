29365df2f81abc823fdc2777787336e6d41a4405
Arthur Teixeira <stag006@benner.com.br>
2019-07-16 11:28:12 -0300

    colocando a~coes paa remover evento

--- T03/JavaScript/introducao-javascript-inicial/introducao-javascript/js/remover-paciente.js
+++ T03/JavaScript/introducao-javascript-inicial/introducao-javascript/js/remover-paciente.js
@@ -1,3 +1,16 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-console.log(pacientes);
+var tabela = document.querySelector("table");
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event) {
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover 
+    paiDoAlvo.remove();
+});
+// pacientes.forEach(function(paciente) {
+//  paciente.addEventListener("dblclick", function() {
+//        this.remove();
+//    });
+// });
