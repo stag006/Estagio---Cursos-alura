bb20a39a2d08f7c88bb8f09b7949f2e3b408ae35
Arthur Teixeira <stag006@benner.com.br>
2019-07-19 10:31:53 -0300

    comit da aula 3 e 4

--- T03/jQuery- Domine a biblioteca mais popular do mercado parte 1/alura-typer/public/js/main.js
+++ T03/jQuery- Domine a biblioteca mais popular do mercado parte 1/alura-typer/public/js/main.js
@@ -1,18 +1,60 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
+var tempoInicial = $("#tempo-digitacao").text();
 
-tamanhoFrase.text(numPalavras);
+$(document).ready(function(){
+    atualizaTamnhoFrase();
+    inicializaContadores();
+    inicializaCronometro();            
+    $("#botao-reiniciar").click(reiniciaJogo);    
+});
 
+function atualizaTamnhoFrase(){
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+    
+}
 
 var campo = $(".campo-digitacao");
-campo.on("input", function(){
-    var conteudo = campo.val();
-    
-    var qtdPalavras = conteudo.split(" ").length;    
-    $("#contador-palavras").text(qtdPalavras);
-    
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
-});
+
+function inicializaContadores(){
+        campo.on("input", function(){
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length -1;    
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
+
+
+function inicializaCronometro() {
+    var tempoRestante =  $("#tempo-digitacao").text();
+    campo.one("focus",function(){
+        var cronometroID = setInterval(function(){
+            tempoRestante--;        
+            $("#tempo-digitacao").text(tempoRestante);
+            if(tempoRestante < 1){
+                campo.attr("disabled",true);
+                clearInterval(cronometroID);
+            }
+        },1000);
+    });
+}
+
+
+//$("#botao-reiniciar").on("click", function(){
+//    console.log("Botao clicado")
+//);
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text('0');
+    $("#contador-caracteres").text('0');
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();    
+}
 
