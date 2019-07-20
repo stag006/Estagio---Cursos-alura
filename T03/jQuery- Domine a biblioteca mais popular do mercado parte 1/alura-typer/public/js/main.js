ed249645559354ff96c8e18e14cecf071af7e7ae
Arthur Teixeira <stag006@benner.com.br>
2019-07-20 13:09:59 -0300

    criando tabelas

--- T03/jQuery- Domine a biblioteca mais popular do mercado parte 1/alura-typer/public/js/main.js
+++ T03/jQuery- Domine a biblioteca mais popular do mercado parte 1/alura-typer/public/js/main.js
@@ -1,10 +1,12 @@
 var tempoInicial = $("#tempo-digitacao").text();
+var campo = $(".campo-digitacao");
 
 $(function(){
     console.log("Pagina Carregou")
     atualizaTamnhoFrase();
     inicializaContadores();
     inicializaCronometro();            
+    inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);    
 });
 
@@ -40,9 +42,28 @@ function inicializaCronometro() {
             if(tempoRestante < 1){
                 campo.attr("disabled",true);
                 clearInterval(cronometroID);
-                campo.toggleClass("campo-desativado")
+                campo.toggleClass("campo-desativado");
             }
         },1000);
+    });
+}
+
+function inicializaMarcadores(){
+    var frase = $(".frase").text();
+    campo.on("input",function(){
+        var digitado = campo.val();
+        var comparavel = frase.substr(0,digitado.length);
+        console.log("digitado:" + digitado);
+        console.log("frase C.:" + comparavel);
+
+        if(frase.startsWith){
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        }
+        else{
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
     });
 }
 
@@ -50,14 +71,15 @@ function inicializaCronometro() {
 //$("#botao-reiniciar").on("click", function(){
 //    console.log("Botao clicado")
 //);
-
 function reiniciaJogo(){
     campo.attr("disabled", false);
     campo.val("");
-    $("#contador-palavras").text('0');
-    $("#contador-caracteres").text('0');
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text(tempoInicial);
     inicializaCronometro();
     campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
 }
 
