78f36fdead036a74949f30b5d2f51ad63e90fed5
Arthur Teixeira <stag006@benner.com.br>
2019-07-22 11:06:15 -0300

    trabalhando com selectize

--- T03/jQuery- Domine a biblioteca mais popular do mercado parte 1/alura-typer/public/js/slider.js
+++ T03/jQuery- Domine a biblioteca mais popular do mercado parte 1/alura-typer/public/js/slider.js
@@ -1,3 +1,9 @@
 $(function(){
-    $(".slider").slick();
+    $(".slider").slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
 });
