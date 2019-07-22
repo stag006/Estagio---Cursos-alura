ee017d2e8ed07191c826c807f8d374db18cf54c9
Arthur Teixeira <stag006@benner.com.br>
2019-07-22 19:26:25 -0300

    jogo

---
+++ T03/JavaScript - projeto final/01-gallows/gallows/js/jogo.js
@@ -0,0 +1,21 @@
+var criaJogo = function() {
+
+    var setPalavraSecreta = function (palavra) {
+        console.log('falta implementar');    
+    };
+
+    var getLacunas = function () {
+        console.log('falta implementar');    
+    };
+
+    var getEtapa = function () {
+        console.log('falta implementar');    
+    };
+
+    return {
+
+        setPalavraSecreta: setPalavraSecreta, 
+        getLacunas: getLacunas,
+        getEtapa: getEtapa
+    };
+};
