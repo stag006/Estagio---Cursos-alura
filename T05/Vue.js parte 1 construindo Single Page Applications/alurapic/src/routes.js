627b27258394bbd798cb3771aa1b443029ab791d
Arthur Teixeira <stag006@benner.com.br>
2019-07-26 12:01:17 -0300

    configurando rotas

---
+++ T05/Vue.js parte 1 construindo Single Page Applications/alurapic/src/routes.js
@@ -0,0 +1,7 @@
+import Cadastro from './componentes/cadastro/Cadastro.vue';
+import Home from './componentes/home/Home.vue';
+
+export const routes = [
+    {path: '', component: Home},
+    {path: '/Cadastro', component: Cadastro}
+];
