4a1aea40143ce24fdc1d4d8e7611d2c99549a9d7
Arthur Teixeira <stag006@benner.com.br>
2019-07-26 11:48:39 -0300

    configura rotas

---
+++ T05/Vue.js parte 1 construindo Single Page Applications/alurapic/src/componentes/home/Home.vue
@@ -0,0 +1,86 @@
+<template>
+  <div >
+    <h1 class="titulo">{{ titulo }}</h1>
+
+    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre por parte do titulo">
+    {{ filtro }}  
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+
+      <meu-painel :titulo="foto.titulo">
+        <imagem-responsiva :url="foto.url" :titulo="foto.titulo">
+      </meu-painel>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from './shared/painel/painel.vue';
+import ImagemResponsilva from './shared/imagem-responsiva/ImagemResponsiva.vue';
+
+export default {
+  'meu-painel' : Painel,
+  'imagem-responsiva': imagemResposiva
+},
+
+data() {
+
+  return {
+
+    titulo: 'Alurapic',
+    fotos: [],
+    filtro: ''
+  }
+}
+
+computed: {
+  fotoComFiltro() {
+
+    if(this.filtro) {
+      let exp = new RegExp(this.filtro.trim(), 'i');
+      return this.fotos.filter(foto => exp.test(foto.titulo));
+    }else{
+      return this.fotos;
+    }
+  }
+},
+
+created() {
+  this.$http
+    .get('http://localhost:3000/v1/fotos')
+    .then(res => res.json())
+    .then(fotos => this.fotos = fotos, err => console.log(err));
+}
+  
+
+
+</script>
+
+<style>
+
+  .titulo {
+    text-align: center;
+  }
+
+  .centralizado {
+
+      text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .filtro {
+    display: block;
+    width: 100%;
+  }
+
+
+</style>
--- T05/Vue.js parte 1 construindo Single Page Applications/alurapic/src/main.js
+++ T05/Vue.js parte 1 construindo Single Page Applications/alurapic/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource';
+import VueRouter from 'vuw-router'
 
 import VueResource from 'vue-resource';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
